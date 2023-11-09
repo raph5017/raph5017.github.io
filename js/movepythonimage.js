41bd61e4708626110b3aea28ee9789da40726c98
Raph Martin <raph5017@gmail.com>
2023-11-09 15:36:07 -0600

    updated python text and images

---
+++ js/movepythonimage.js
@@ -0,0 +1,11 @@
+//cursor hovers on then get image of winter
+function moveon()
+{
+document.getElementById('image').src="images/python2.png";
+}
+
+//cursor hovers off then get image of summer
+function moveoff()
+{
+document.getElementById('image').src="images/python1.png";
+}
